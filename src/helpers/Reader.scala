1b95aebf0085f81a95eb8b9a434f8cb6fd8af2ef
Kamil Lelonek <squixy.sln@gmail.com>
2014-04-02 00:05:39 +0200

    Basic Reader method

--- src/helpers/Reader.scala
+++ src/helpers/Reader.scala
@@ -1,5 +1,14 @@
 package helpers
 
-object Reader {
+import net.sf.mpxj.mpp.MPPReader
 
+object Reader {
+    private lazy val reader = new MPPReader
+
+    def getAllTasks(sourceFile: String) = {
+        val projectFile = reader.read(sourceFile)
+        val projectTasks = projectFile.getAllTasks
+        if (projectTasks.get(0).getName.equals("clone")) projectTasks.remove(0)
+        projectTasks
+    }
 }
