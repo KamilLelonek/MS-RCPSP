1e5146834fe43987c8abbede3a41a789a54afe67
Kamil Lelonek <squixy.sln@gmail.com>
2014-04-02 00:05:51 +0200

    Basic Printer method

--- src/helpers/Printer.scala
+++ src/helpers/Printer.scala
@@ -1,5 +1,13 @@
 package helpers
 
-object Printer {
+import net.sf.mpxj.Task
+import scala.collection.JavaConversions._
 
+object Printer {
+    def listHierarchy(task: Task, indent: String = ""): Unit = {
+        task.getChildTasks.foreach(child => {
+            println(indent + "Task: " + child.getName)
+            listHierarchy(child, indent + "\t")
+        })
+    }
 }
