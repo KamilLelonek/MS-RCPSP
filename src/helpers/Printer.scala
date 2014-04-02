731e69e711527f227afb0e81bf3619a8fa558b8a
Kamil Lelonek <squixy.sln@gmail.com>
2014-04-02 23:07:01 +0200

    Printer class implemented

--- src/helpers/Printer.scala
+++ src/helpers/Printer.scala
@@ -1,13 +1,22 @@
 package helpers
 
+import scala.collection.JavaConversions.asScalaBuffer
+
+import core.eval.Eval
+import net.sf.mpxj.ProjectFile
 import net.sf.mpxj.Task
-import scala.collection.JavaConversions._
 
 object Printer {
     def listHierarchy(task: Task, indent: String = ""): Unit = {
-        task.getChildTasks.foreach(child => {
+        task.getChildTasks foreach (child => {
             println(indent + "Task: " + child.getName)
             listHierarchy(child, indent + "\t")
         })
     }
+
+    def projectCost(project: ProjectFile) =
+        println("Project cost: " + Eval.getProjectCost(project))
+
+    def projectDuration(project: ProjectFile) =
+        println("Project duration: " + Eval.getProjectDuration(project))
 }
